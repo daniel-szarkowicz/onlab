3820d089cb811c715ed41e86b7104131d211906d
Szarkowicz Dániel <szarkowicz.dani@proton.me>
2024-03-23 22:32:22 +0100

    failed fallback solutions

--- src/phong-fs.glsl
+++ src/phong-fs.glsl
@@ -43,7 +43,48 @@ float calculate_shadow(uint i, vec3 normal, vec3 light_dir) {
   float exp_cz = texture(directional_lights[i].shadow_map, map_coords.xyz).r;
   float exp_minuscd = exp(-80 * map_coords.w);
   float shadow = exp_cz * exp_minuscd;
-  return clamp(shadow, 0, 1);
+  if (shadow > 1.05) {
+    shadow = 0;
+    vec2 texel_size = 1.0/vec2(textureSize(directional_lights[i].shadow_map, 0));
+    vec2 offset = mod(map_coords.xy, texel_size);
+    float x = map_coords.x;
+    float y = map_coords.y;
+    float layer = map_coords.z;
+    float x1 = map_coords.x - offset.x;
+    float x2 = x1 + texel_size.x;
+    float y1 = map_coords.y - offset.y;
+    float y2 = y1 + texel_size.y;
+    shadow = 1/(texel_size.x * texel_size.y) * (
+      min(texture(directional_lights[i].shadow_map, vec3(x1, y1, layer)).r * exp_minuscd, 1) * (x2 - x) * (y2 - y) +
+      min(texture(directional_lights[i].shadow_map, vec3(x2, y1, layer)).r * exp_minuscd, 1) * (x - x1) * (y2 - y) +
+      min(texture(directional_lights[i].shadow_map, vec3(x1, y2, layer)).r * exp_minuscd, 1) * (x2 - x) * (y - y1) +
+      min(texture(directional_lights[i].shadow_map, vec3(x2, y2, layer)).r * exp_minuscd, 1) * (x - x1) * (y - y1)
+    );
+    // float samples = 8;
+    // float pi = 3.141592653589793;
+    // for (float n = samples; n > 0; n -= 1) {
+    //   for (float m = 1; m <= samples; m += 1) {
+    //     float u = n/samples;
+    //     float v = m/samples;
+    //     float x = sqrt(u) * cos(2*pi * v) * texel_size.x;
+    //     float y = sqrt(u) * sin(2*pi * v) * texel_size.y;
+    //     exp_cz = texture(
+    //       directional_lights[i].shadow_map, map_coords.xyz + vec3(x, y, 0)
+    //     ).r;
+    //     shadow += min(exp_cz * exp_minuscd, 1);
+    //   }
+    // }
+    // for (float x = -1; x <= 1; x += 1) {
+    //   for (float y = -1; y <= 1; y += 1) {
+    //     exp_cz = texture(
+    //       directional_lights[i].shadow_map, map_coords.xyz + vec3(x, y, 0) * vec3(texel_size, 0)
+    //     ).r;
+    //     shadow += min(exp_cz * exp_minuscd, 1);
+    //   }
+    // }
+    // shadow = shadow / 9;
+  }
+  return shadow;
 }
 
 void main() {
@@ -57,8 +98,12 @@ void main() {
     float cosd = max(dot(N, H), 0);
     float cosa = max(dot(N, V), 0);
     float shadow = calculate_shadow(i, N, L);
+    if (shadow > 1.05) {
+      frag_color = vec4(1, 0, 0, 1);
+      return;
+    }
     color += ka * (0.9 + cosa * 0.1) * directional_lights[i].ambient_color;
-    color += shadow * (kd * cost + ks * pow(cosd, shine))
+    color += clamp(shadow, 0, 1) * (kd * cost + ks * pow(cosd, shine))
       * directional_lights[i].emissive_color;
   }
   frag_color = vec4(color, 1);
