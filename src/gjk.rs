52fdc7239ea0fa6b05fc2c584a5995ed10d45f9e
Szarkowicz Dániel <szarkowicz.dani@proton.me>
2024-04-01 18:22:33 +0200

    reverse triangle winding when choosing the negative normal direction and fix two indexing typos

--- src/gjk.rs
+++ src/gjk.rs
@@ -26,14 +26,29 @@ pub fn gjk(a: &impl Support, b: &impl Support) -> bool {
     let mut dir = Vec3::new(random(), random(), random());
     let mut s = Vec::with_capacity(5);
     loop {
+        // if dir.magnitude() < 0.001 {
+        //     println!("simplex contains origin");
+        //     return true;
+        // }
         assert!(dir.magnitude() > f64::EPSILON);
+        // dir.normalize_mut();
         let new_point = SimplexPoint::new(a, b, &dir);
-        if new_point.diff.dot(&dir) < -f64::EPSILON {
+        // println!("dir dot {:?}", new_point.diff.dot(&dir.normalize()));
+        if new_point.diff.dot(&dir) < 0.001 {
             return false;
             // todo!("calculate closest points");
         }
+        // println!("distance form origin: {}", new_point.diff.magnitude());
+        // if new_point.diff.magnitude() < 0.001 {
+        //     dbg!("close to the origin");
+        //     return true;
+        // }
         s.push(new_point);
         // dbg!(s.len());
+
+        // for p in &s {
+        //     println!("({}, {}, {}),", p.diff.x, p.diff.y, p.diff.z);
+        // }
         let contains_origin;
         (s, dir, contains_origin) = best_simplex(s);
         // dbg!(s.len(), dir);
@@ -71,11 +86,12 @@ fn best_simplex(mut s: Vec<SimplexPoint>) -> (Vec<SimplexPoint>, Vec3, bool) {
                 let dir = (s[1].diff - s[0].diff)
                     .cross(&-s[0].diff)
                     .cross(&(s[1].diff - s[0].diff));
+                // println!("ac");
                 return (s, dir, false);
             }
 
             // háromszögből kifele mutat, bc-re merőleges
-            let bc_perp = (s[1].diff - s[2].diff).cross(&abc_perp);
+            let bc_perp = (s[2].diff - s[1].diff).cross(&abc_perp);
             // ha az origó egy irányba van az oldal normáljával
             if bc_perp.dot(&-s[2].diff) > 0.0 {
                 // a-t kivesszük, mert nem kell
@@ -83,6 +99,7 @@ fn best_simplex(mut s: Vec<SimplexPoint>) -> (Vec<SimplexPoint>, Vec3, bool) {
                 let dir = (s[1].diff - s[0].diff)
                     .cross(&-s[0].diff)
                     .cross(&(s[1].diff - s[0].diff));
+                // println!("bc");
                 return (s, dir, false);
                 // a háromszögön belül vagyunk
             }
@@ -92,8 +109,21 @@ fn best_simplex(mut s: Vec<SimplexPoint>) -> (Vec<SimplexPoint>, Vec3, bool) {
                 (s, abc_perp, false)
             // -abc_perp irányba van az origó
             } else {
+                s.reverse();
                 (s, -abc_perp, false)
             }
+            // let dot = abc_perp.dot(&-s[2].diff);
+            // if dot < -f64::EPSILON {
+            //     // println!("origin \"below\" triangle");
+            //     s.reverse();
+            //     (s, -abc_perp, false)
+            // } else if dot <= f64::EPSILON {
+            //     // println!("triangle contains origin");
+            //     (s, Vec3::zeros(), true)
+            // } else {
+            //     // println!("origin \"above\" triangle");
+            //     (s, abc_perp, false)
+            // }
         }
         4 => {
             // Az origó nem lehet az abc háromszög "alatt" és a d pont "fölött".
@@ -106,7 +136,12 @@ fn best_simplex(mut s: Vec<SimplexPoint>) -> (Vec<SimplexPoint>, Vec3, bool) {
             let abd_perp =
                 (s[1].diff - s[0].diff).cross(&(s[3].diff - s[0].diff));
             // Ha abd síkján kívül van
+            assert!(
+                abd_perp.dot(&(s[2].diff - s[3].diff)) < 0.0,
+                "abd={abd_perp:?}"
+            );
             if abd_perp.dot(&-s[3].diff) > 0.0 {
+                // println!("origin \"above\" abd");
                 s.remove(2);
                 return tetrahedron_triangle_subcheck(s, abd_perp);
             }
@@ -115,8 +150,13 @@ fn best_simplex(mut s: Vec<SimplexPoint>) -> (Vec<SimplexPoint>, Vec3, bool) {
             //   Ha ott van, akkor lehet bd vagy a cd oldalon kívül
             //   vagy a bcd háromszög "fölött".
             let bcd_perp =
-                (s[2].diff - s[3].diff).cross(&(s[3].diff - s[1].diff));
+                (s[2].diff - s[1].diff).cross(&(s[3].diff - s[1].diff));
+            assert!(
+                bcd_perp.dot(&(s[0].diff - s[3].diff)) < 0.0,
+                "bcd={bcd_perp:?}"
+            );
             if bcd_perp.dot(&-s[3].diff) > 0.0 {
+                // println!("origin \"above\" bcd");
                 s.remove(0);
                 return tetrahedron_triangle_subcheck(s, bcd_perp);
             }
@@ -126,7 +166,12 @@ fn best_simplex(mut s: Vec<SimplexPoint>) -> (Vec<SimplexPoint>, Vec3, bool) {
             //   vagy a bcd háromszög "fölött".
             let cad_perp =
                 (s[0].diff - s[2].diff).cross(&(s[3].diff - s[2].diff));
+            assert!(
+                cad_perp.dot(&(s[1].diff - s[3].diff)) < 0.0,
+                "cad={cad_perp:?}"
+            );
             if cad_perp.dot(&-s[3].diff) > 0.0 {
+                // println!("origin \"above\" cad");
                 s.remove(1);
                 let (s1, s2) = s.split_at_mut(1);
                 std::mem::swap(&mut s1[0], &mut s2[0]);
@@ -148,6 +193,7 @@ fn tetrahedron_triangle_subcheck(
 ) -> (Vec<SimplexPoint>, Vec3, bool) {
     assert!(s.len() == 3);
     // xd-re merőleges, kifelé mutat
+    // dbg!(&xyd_perp);
     let xd_perp = xyd_perp.cross(&(s[2].diff - s[0].diff));
     // xd-n kívül van
     if xd_perp.dot(&-s[2].diff) > 0.0 {
@@ -155,6 +201,7 @@ fn tetrahedron_triangle_subcheck(
         let dir = (s[1].diff - s[0].diff)
             .cross(&-s[0].diff)
             .cross(&(s[1].diff - s[0].diff));
+        // println!("xd");
         return (s, dir, false);
     }
 
@@ -166,6 +213,7 @@ fn tetrahedron_triangle_subcheck(
         let dir = (s[1].diff - s[0].diff)
             .cross(&-s[0].diff)
             .cross(&(s[1].diff - s[0].diff));
+        // println!("yd");
         return (s, dir, false);
     }
 
